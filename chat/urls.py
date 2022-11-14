e5e7e430b1837087c840a8e693e351744384bdeb
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-11-14 16:28:20 +0500

    Login and Registration Form added

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,6 @@ app_name = 'chat'
 # url patterns path
 urlpatterns = [
     path('', views.index,  name='index'),
+    path('register', views.register,  name='register'),
+    path('login', views.login,  name='login'),
 ]
